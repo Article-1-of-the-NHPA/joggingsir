f2d1cf9305847ac67f45689d6a6371a0628c2e4a
PangHee1027 <leejeahyeon10271027@gmail.com>
2023-11-19 00:24:24 +0900

    jaehyeon's ui 0.2

--- lib/login.dart
+++ lib/login.dart
@@ -8,26 +8,26 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                TextField(
-                  controller: _usernameController,
-                  decoration: const InputDecoration(labelText: '아이디'),
-                ),
-                const SizedBox(height: 16.0),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(labelText: '비밀번호'),
-                ),
-                const SizedBox(height: 32.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: ElevatedButton(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(labelText: '아이디'),
+                  ),
+                  const SizedBox(height: 16.0),
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(labelText: '비밀번호'),
+                  ),
+                  const SizedBox(height: 32.0),
+                  ElevatedButton(
                     onPressed: () {
                       // 로그인 기능이 제거된 UI 버전입니다.
                     },
@@ -44,72 +44,70 @@ class LoginPage extends StatelessWidget {
                       style: TextStyle(fontSize: 16.0, color: Colors.white),
                     ),
                   ),
-                ),
-                const SizedBox(height: 32.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        // 기능이 제거된 UI 버전에서는 동작하지 않습니다.
-                        print('아이디/비밀번호 찾기');
-                      },
-                      child: const Text(
-                        '아이디/비밀번호 찾기',
+                  const SizedBox(height: 32.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          // 기능이 제거된 UI 버전에서는 동작하지 않습니다.
+                          print('아이디/비밀번호 찾기');
+                        },
+                        child: const Text(
+                          '아이디/비밀번호 찾기',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                      const SizedBox(width: 16.0),
+                      GestureDetector(
+                        onTap: () {
+                          // 기능이 제거된 UI 버전에서는 동작하지 않습니다.
+                          print('회원가입');
+                        },
+                        child: const Text(
+                          '회원가입',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 128.0),
+                  const Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(width: 8.0),
+                      Text(
+                        '간편 로그인',
                         style: TextStyle(color: Colors.grey),
                       ),
-                    ),
-                    const SizedBox(width: 16.0),
-                    GestureDetector(
-                      onTap: () {
-                        // 기능이 제거된 UI 버전에서는 동작하지 않습니다.
-                        print('회원가입');
-                      },
-                      child: const Text(
-                        '회원가입',
-                        style: TextStyle(color: Colors.grey),
+                      SizedBox(width: 8.0),
+                      Expanded(
+                        child: Divider(
+                          color: Colors.grey,
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 64.0),
-                const Row(
-                  children: [
-                    Expanded(
-                      child: Divider(
-                        color: Colors.grey,
-                      ),
-                    ),
-                    SizedBox(width: 8.0),
-                    Text(
-                      '간편 로그인',
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                    SizedBox(width: 8.0),
-                    Expanded(
-                      child: Divider(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 32,),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
+                    ],
+                  ),
+                  const SizedBox(height: 32,),
+                  Container(
                     width: double.infinity,
                     child: Image.asset(
-                      "kakao_login_medium_wide.png",
+                      "kakao_login.png",
                       fit: BoxFit.cover,
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
 
