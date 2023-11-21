ae627609da643d03070eefea598f66b9d91cb193
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-11-21 18:42:08 +0900

    Feat: UserInfo 위젯 추가

--- lib/views/home/my_page_view.dart
+++ lib/views/home/my_page_view.dart
@@ -1,16 +1,21 @@
-import '../widgets/main_app_bar.dart';
 import 'package:flutter/material.dart';
+import '../widgets/main_app_bar.dart';
+import '../widgets/user-info.dart';
 
 void main() {
   runApp(const MyPage());
 }
+
 class MyPage extends StatelessWidget {
   const MyPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainAppBar(title: 'MyPage'), // MainAppBar 위젯을 사용
+      home: Scaffold(
+        appBar: MainAppBar(title: 'MyPage'),
+        body: UserInfo()
+      ),
     );
   }
 }
---
+++ lib/views/widgets/user-info.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class UserInfo extends StatelessWidget {
+  const UserInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(18.0),
+        child: SizedBox(
+          height: 260,
+          width: 357,
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30.0),
+              side: BorderSide(color: Colors.black),
+            ),
+            elevation: 4.0,
+            color: Colors.white,
+            child: Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Container(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 100.0,
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            color: const Color(0xff7c94b6),
+                            image: DecorationImage(
+                              image: NetworkImage('https://mblogthumb-phinf.pstatic.net/MjAyMjAyMDdfMjEy/MDAxNjQ0MTk0Mzk2MzY3.WAeeVCu2V3vqEz_98aWMOjK2RUKI_yHYbuZxrokf-0Ug.sV3LNWlROCJTkeS14PMu2UBl5zTkwK70aKX8B1w2oKQg.JPEG.41minit/1643900851960.jpg?type=w800'),
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.all( Radius.circular(50.0)),
+                            border: Border.all(
+                              color: const Color(0xffff7777),
+                              width: 4.0,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    'User',
+                                    style: TextStyle(fontSize: 17),
+                                  ),
+                                  SizedBox(width: 6),
+                                  Icon(
+                                    Icons.edit, // Use Icons.edit for a pencil icon
+                                    size: 18,
+                                  ),
+                                  SizedBox(width: 6),
+                                  TextButton(
+                                    onPressed: () {},
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: const Color(0xFFFF6060).withOpacity(0.3), // Set background color to FF6060
+                                      padding: EdgeInsets.symmetric(horizontal: 22, vertical: 10), // Set width and height
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(30.0), // Set border-radius to 30
+                                      ),
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          'Red',
+                                          style: GoogleFonts.getFont(
+                                            'Pattaya',
+                                            textStyle: const TextStyle(color: Color(0xffff6060), fontSize: 13),
+                                          ),
+                                        ),
+                                        Icon(
+                                          Icons.arrow_forward_ios,
+                                          size: 18,
+                                          color: Color(0xffff6060)
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                'admin@naver.com',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              SizedBox(height: 6),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    '리워드',
+                                    style: TextStyle(fontSize: 13),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        '120',
+                                        style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                                      ),
+                                      Icon(
+                                        Icons.arrow_forward_ios, // You can replace this with your '>' icon
+                                        size: 18,
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    '찜한 경로',
+                                    style: TextStyle(fontSize: 13),
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        '5',
+                                        style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
+                                      ),
+                                      Icon(
+                                        Icons.arrow_forward_ios, // You can replace this with your '>' icon
+                                        size: 18,
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Text(
+                          '최근 방문 경로',
+                          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black.withOpacity(0.5)),
+                        ),
+                        Text(
+                          '리뷰 관리',
+                          style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.black.withOpacity(0.5)),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 9),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        CircleAvatar(
+                          radius: 50 / 2,
+                          backgroundImage: NetworkImage('https://mblogthumb-phinf.pstatic.net/MjAyMjAyMDdfMjEy/MDAxNjQ0MTk0Mzk2MzY3.WAeeVCu2V3vqEz_98aWMOjK2RUKI_yHYbuZxrokf-0Ug.sV3LNWlROCJTkeS14PMu2UBl5zTkwK70aKX8B1w2oKQg.JPEG.41minit/1643900851960.jpg?type=w800'),
+                        ),
+                        Text('|', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w100)),
+                        Text(
+                          '3',
+                          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
